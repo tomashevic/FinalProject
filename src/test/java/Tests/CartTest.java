e14bb2c9c83728574f085cd2c26479a13ddcf23b
tomashevic <lazartomasevic@gmail.com>
2024-10-29 18:22:35 +0100

    Added @AfterMethod tearDown

--- src/test/java/Tests/CartTest.java
+++ src/test/java/Tests/CartTest.java
@@ -4,6 +4,7 @@ import Base.BaseTest;
 import Pages.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -160,6 +161,11 @@ public class CartTest extends BaseTest {
         Assert.assertTrue(isDisplayed(cartPage.cartItemName));
         Assert.assertTrue(isDisplayed(cartPage.cartItemPrice));
 
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 
 }
