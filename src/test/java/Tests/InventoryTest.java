51f73dfb8b2addf3a57661653d0dc781de126dcb
tomashevic <lazartomasevic@gmail.com>
2024-10-29 18:16:42 +0100

    Added tearDown method

--- src/test/java/Tests/InventoryTest.java
+++ src/test/java/Tests/InventoryTest.java
@@ -7,6 +7,7 @@ import Pages.InventoryPage;
 import Pages.LoginPage;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -102,5 +103,10 @@ public class InventoryTest extends BaseTest {
         Assert.assertFalse(isDisplayed(cartPage.itemThreeQuantity));
         Assert.assertFalse(isDisplayed(cartPage.cartItemThreePrice));
 
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
